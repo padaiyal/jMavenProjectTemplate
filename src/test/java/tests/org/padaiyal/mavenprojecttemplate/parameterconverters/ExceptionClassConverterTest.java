8ba3f2be62a4045d2f85522700868b82bcfb63d1
Silvs <silviasiu1998@gmail.com>
2021-01-06 17:29:55 -0500

    Add Sample Maven Project
    Closes #6.
    Checked out submodules before maven build in GitHub actions.
    Added sample Maven Project.
    Added jPopper as a submodule.
    Add a sample WordCounter.

---
+++ src/test/java/tests/org/padaiyal/mavenprojecttemplate/parameterconverters/ExceptionClassConverterTest.java
@@ -0,0 +1,48 @@
+package tests.org.padaiyal.mavenprojecttemplate.parameterconverters;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.padaiyal.mavenprojecttemplate.parameterconverters.ExceptionClassConverter;
+
+/**
+ * Tests the functionality of ExceptionClassConverter.
+ */
+public class ExceptionClassConverterTest {
+
+  /**
+   * Tests org.padaiyal.mavenprojecttemplate.parameterconverters.ExceptionClassConverter
+   * ::convertExceptionNameToClass() with invalid inputs.
+   */
+  @Test
+  public void testConvertExceptionNameToClassWithInvalidInputs() {
+    // Null input
+    Assertions.assertThrows(
+        NullPointerException.class,
+        () -> ExceptionClassConverter.convertExceptionNameToClass(null));
+
+    // Unsupported/Unknown class
+    Assertions.assertThrows(
+        ArgumentConversionException.class,
+        () -> ExceptionClassConverter.convertExceptionNameToClass(
+            "UnknownException"
+        )
+    );
+  }
+
+  /**
+   * Tests org.padaiyal.mavenprojecttemplate.parameterconverters.ExceptionClassConverter
+   * ::convertExceptionNameToClass() withvalid inputs.
+   */
+  @Test
+  public void testConvertExceptionNameToClassWithValidInputs() {
+    Assertions.assertEquals(
+        NullPointerException.class,
+        ExceptionClassConverter.convertExceptionNameToClass("NullPointerException.class")
+    );
+    Assertions.assertEquals(
+        IllegalArgumentException.class,
+        ExceptionClassConverter.convertExceptionNameToClass("IllegalArgumentException.class")
+    );
+  }
+}
